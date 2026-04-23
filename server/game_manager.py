cf6ab9317badf16e78940390f1337892a5d82fa3
benmini <benjamin0901@gmail.com>
2026-04-23 14:33:33 +0800

    Allow game start with minimum 2 players (max 4)

--- server/game_manager.py
+++ server/game_manager.py
@@ -12,7 +12,8 @@ class GameManager:
         return game_id
 
     def create_game_if_ready(self, game_id, GameClass, players):
-        if self.games.get(game_id) is None and len(players) == 4:
+        # Start game when at least 2 players have joined (max 4)
+        if self.games.get(game_id) is None and len(players) >= 2:
             self.games[game_id] = GameClass(players)
 
     def get_game(self, game_id):
